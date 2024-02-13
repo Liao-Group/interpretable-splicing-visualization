8f8f0fdd2d4c34b09c1109f6a959cbd47e7add35
shiwen59 <zhushiwen007@icloud.com>
2024-02-13 01:55:38 -0500

    Completed csv -> json function
    Based on the previous single column reader, iterate through other incl and skip columns, format, and output as a tree structure. The function now takes the file path of the csv file of an exon and will be able to produce the tree structure that is essential for building the hierarchical bar chart later on.

--- csvToJson/csvToJson.js
+++ csvToJson/csvToJson.js
@@ -3,23 +3,26 @@ const fs = require('fs');
 const Papa = require('papaparse');
 
 // Function that extract the information of a single feature of a single exon
-function readSingleFeature(filePath) {
+function readSingleFeature(filePath, colName, callback) {
     // To read CSV file
     fs.readFile(filePath, 'utf8', (err, fileContent) => {
         if(err) {
             console.error('Error reading the file', err);
+            return;
         }
         Papa.parse(fileContent, {
             header: true,
             dynamicTyping: true,
             complete: (results) => {
                 let filteredData = results.data
-                    .filter(row => row['incl_1'] > 0.001)
+                    .filter(row => row[colName] > 0.001)
                     .map(row => ({
                         position: row['position'],
-                        incl_1: row['incl_1']
+                        value: row[colName]
                     }));
-                if(filteredData.length === 0) {
+                callback(filteredData, colName);
+
+                /***if(filteredData.length === 0) {
                     console.log("No 'incl_1' values found greater than E-3.");
                 }
                 else {
@@ -31,22 +34,75 @@ function readSingleFeature(filePath) {
                         if(error) {console.error('Error writing JSON to file: ', error);}
                         else {console.log('Formatted data written successfully to ',OutputPath);}
                     })
-                }
+                }***/
             }
         })
     })
 }
 
 // Function that transform the data to hierarchical format
-function formatData(filteredData) {
+function formatData(filteredData, colName) {
     return {
-        name: 'inclu_1',
+        name: colName,
         children: filteredData.map(row => ({
-            name: 'position_${row.position}',
-            value: row.incl_1
+            name: `position_${row.position}`,
+            value: row.value
         }))
     };
 }
 
+// Function that iterate through different columns
+function collectAllFeatures(filePath, cols, callback) {
+    // To create the structure that holds the children features
+    const allData = {
+        name: 'Splicing Data',
+        children: [
+            {
+                name: 'incl',
+                children: []
+            },
+            {
+                name: 'skip',
+                children: []
+            }
+        ]
+    };
+    let colsProcessed = 0;
+    cols.forEach(colName => {
+        readSingleFeature(filePath, colName, (filteredData, name) => {
+            // To call formatting function
+            const formattedData = formatData(filteredData, colName);
+            // To add features to different groups
+            if (name.startsWith('incl')) {
+                allData.children[0].children.push(formattedData);
+            } else if (name.startsWith('skip')) {
+                allData.children[1].children.push(formattedData);
+            }
+
+            colsProcessed++;
+            if(colsProcessed === cols.length) {callback(allData);}
+        })
+    })
+}
+
+// Funtion that write the combined data to a JSON file
+function writeToFile(data, outputPath) {
+    fs.writeFile(outputPath, JSON.stringify(data, null, 2), 'utf8', error => {
+        if(error) {
+            console.error('Error writing JSON to file:', error);
+        }
+        else {
+            console.log('Data written successfully to', outputPath);
+        }
+    });
+}
+
 // To run functions
-readSingleFeature(path);
+const colsToProcess = ['incl_1', 'incl_2', 'incl_3', 'incl_4', 'incl_5', 'incl_6', 'incl_7', 
+    'skip_7', 'skip_8', 'skip_9', 'skip_10', 'skip_11', 'skip_12', 'skip_s', 'skip_p'];
+
+const outputPath = path.replace('.csv', '_all_features_formatted.json');
+
+collectAllFeatures(path, colsToProcess, allData => {
+    writeToFile(allData, outputPath);
+});
