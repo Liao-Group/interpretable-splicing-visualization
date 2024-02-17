72c9764ec71d27311ea8d6db265c51d1590422df
shiwen59 <zhushiwen007@icloud.com>
2024-02-16 23:28:11 -0500

    Draw static bar chart
    Created a bar chart with sample data

--- visualization/main.js
+++ visualization/main.js
@@ -20,22 +20,67 @@ let background_model_color = "#e4f2e3"
 let line_color = "#6b6b6b";
 let strength_difference_color = "#dad7cd";
 
+// Temp sample data
+const data = [
+    {category: 'incl_1', value: 30},
+    {category: 'incl_2', value: 80},
+    {category: 'incl_3', value: 45},
+    {category: 'skip_4', value: 60},
+    {category: 'skip_5', value: 40},
+    {category: 'skip_6', value: 70},
+    {category: 'skip_s', value: 50}
+  ];
+
 // Draw background
-let padding = 5;
+
+let padding = 10;
 
 let svg_plot = svg.append("g")
-                    .attr("id", "plot")
-                    .attr("transform", "translate(" + padding + "," + padding + ")");
+  .attr("id", "plot")
+  .attr("transform", "translate(" + padding + "," + padding + ")");
 
 svg_plot.append("rect")
-            .attr("class", "background")
-            .attr("fill", background_color)
-            .attr("width", width-2*padding)
-            .attr("height", height/2-2*padding);
+  .attr("class", "background")
+  .attr("fill", background_color)
+  .attr("width", width - 2 * padding)
+  .attr("height", height / 2 - 2 * padding);
 
-// Draw position plot
-d3.csv("./data/deciphering_rna_splicing.csv", 
+// Draw static bar chart
+
+const margin = {top: 50, right: 50, bottom: 50, left: 50};
+const chartWidth = width - 2 * padding - margin.left - margin.right;
+const chartHeight = height / 2 - 2 * padding - margin.top - margin.bottom;
+
+const x = d3.scaleBand()
+  .rangeRound([0, chartWidth])
+  .padding(0.1)
+  .domain(data.map(d => d.category));
+
+const y = d3.scaleLinear()
+  .rangeRound([chartHeight, 0])
+  .domain([0, d3.max(data, d => d.value)]);
+
+let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+g.selectAll(".bar")
+  .data(data)
+  .enter().append("rect")
+  .attr("class", "bar")
+  .attr("x", d => x(d.category))
+  .attr("y", d => y(d.value))
+  .attr("width", x.bandwidth())
+  .attr("height", d => chartHeight - y(d.value))
+  .attr("fill", d => d.category.startsWith('incl') ? inclusion_color : skipping_color);
+
+g.append("g")
+  .attr("transform", `translate(0, ${chartHeight})`)
+  .call(d3.axisBottom(x));
+
+g.append("g")
+  .call(d3.axisLeft(y));
+
+/***d3.csv("./data/deciphering_rna_splicing.csv", 
 function(data) {
     console.log(data);
     //Add bar chart here
-})
+})***/
