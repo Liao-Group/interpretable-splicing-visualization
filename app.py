8d14718f6dc18789811cbfc882ee38317cd6c94c
Mateus Silva Aragao <msa8779@nyu.edu>
2024-09-13 12:55:59 -0400

    commiting for deploy

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def get_data():
         return jsonify({"error": "Data not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5050,debug=True)
 
